b82d12ed4f6ef1cd3558fd0b473139a432915bbe
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-18 01:28:50 +0530

    added fake accounts, nonce, and builed a transaction

--- fetch_block_ganache.py
+++ fetch_block_ganache.py
@@ -12,4 +12,28 @@ else:
     print("Failed to connect to Ganache")
 
 
+# creating fake accounts
+acc_1 = "0x9B0369515D9Ed16FEc420e3370BCeE87220E4448"
+acc_2 = "0x0C18e7dF4209E4A3555F2cc2441dB2F38ff1284a"
 
+private_key_acc_1 = "0xb0a2801399e24281c4e96b98fd7c6941ebbf18df2b9d5a9b597575ffbfabeda5" # private key added from 
+
+# get the nonce 
+nonce = Web3.eth.get_transaction_count(acc_1)
+
+# build a transaction
+tx = {
+    'nonce': nonce, # prevents from repeated transacx
+    'to': acc_2,
+    'from': acc_1,
+    'value': Web3.to_wei(2, 'ether'),
+    'gas':  2000000,
+    'gasPrice': Web3.to_wei('50', 'gwei')
+
+}
+
+
+
+# sign a transaction
+# send a transaction
+# get a transaction hash
