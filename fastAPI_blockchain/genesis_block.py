782c72f0dd76dad43a286518d00ff2117be4c313
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-20 02:11:59 +0530

    creating blockchain with FastAPI

---
+++ fastAPI_blockchain/genesis_block.py
@@ -0,0 +1,30 @@
+"""
+
+Genesis block
+{
+    index: 0,
+    timestamp: current time,
+    data: "It all started with me"
+    proof: 3,
+    previous_hash: "0"
+} -> hash() -> 45646asda
+
+
+{
+    index: 1,
+    timestamp: current time,
+    data: "after the genesis block",
+    proof: 2343,
+    previous_hash: 45646asda
+} -> hash() -> 54566asdd
+
+
+{
+    index: 2,
+    timestamp: current time,
+    data: "ends with me",
+    proof: 2134123,
+    previous_hash: 54566asdd
+}
+
+"""
