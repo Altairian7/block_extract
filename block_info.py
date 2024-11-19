39447a3dbb9385c9e2f6687cf3774cd13a45a4d6
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-19 23:06:05 +0530

    fetch latest block and get it's details

--- block_info.py
+++ block_info.py
@@ -4,3 +4,9 @@ from web3 import Web3
 # ethereum se connect kr
 infura_url = 'https://mainnet.infura.io/v3/251315b2f23f4439b86206e0bfd79df3'  # infura id replace kr
 web3 = Web3(Web3.HTTPProvider(infura_url))
+
+
+
+latest_block = web3.eth.block_number    # get Last Finalized Block
+
+print(web3.eth.get_block_transaction_count(latest_block))   # all deatil of latest block
