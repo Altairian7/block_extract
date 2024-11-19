50b78d9435a423c2c72b6bbb7300df7f340027f7
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-19 23:12:14 +0530

    fetch transacitons of a particular block

--- block_info.py
+++ block_info.py
@@ -10,3 +10,7 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 latest_block = web3.eth.block_number    # get Last Finalized Block
 
 print(web3.eth.get_block_transaction_count(latest_block))   # all deatil of latest block
+
+
+block_hash = '0x1e257845d7f7792397aad25bbf9e939df75798f6e469e6db86b3a6edaf4c9d34'
+print(web3.eth.get_transaction_by_block(block_hash, 2))
