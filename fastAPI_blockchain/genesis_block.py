3e59c3fda874a6b535b1194d4bccd39e2511df2d
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-23 23:08:59 +0530

    created blockchain class, and in it added create_block and genesis block function

--- fastAPI_blockchain/genesis_block.py
+++ fastAPI_blockchain/genesis_block.py
@@ -27,4 +27,27 @@ Genesis block
     previous_hash: 54566asdd
 }
 
+
 """
+
+
+import datatime as _dt
+import hashlib as _hashlib
+import json as _json
+
+
+class Blockchain:
+    def __init__(self) -> None:
+        self.chain = list()
+        genesis_block = self._create_block(data="It all started with me", proof=1, previous_hash="0", index=0)
+        self.chain.append(genesis_block)
+        
+    def _create_block(self, data: str, proof: int, previous_hash: str, index: int) -> dict:
+        block = {
+            "index": index,
+            "timestamp": str(_dt.datetime.now()),
+            "data": data,
+            "proof": proof,
+            "previous_hash": previous_hash,
+        }
+    
