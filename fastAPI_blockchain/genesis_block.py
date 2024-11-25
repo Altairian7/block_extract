d6a1c197efcb39a0b5a0e81bc9287aabf5cfdb3e
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-25 21:02:45 +0530

    added is-chain-valid function and again made some changes in mine-block

--- fastAPI_blockchain/genesis_block.py
+++ fastAPI_blockchain/genesis_block.py
@@ -37,19 +37,19 @@ import json as _json
 
 
 class Blockchain:
-    def __init__(self) -> None:
+    def __init__(self):
         self.chain = list()
-        genesis_block = self._create_block(data="It all started with me", proof=1, previous_hash="0", index=0)
+        genesis_block = self._create_block(data="It all started with me", proof=1, previous_hash="0", index=1)
         self.chain.append(genesis_block)
         
     def mine_block(self, data: str) -> dict:
         previous_block = self.get_previous_block()
         previous_proof = previous_block["proof"]
         index = len(self.chain) + 1
-        proof = self._proof_of_work(previous_proof, index, data)
+        proof = self._proof_of_work(previous_proof=previous_proof, index=index, data=data)
         
-        previous_hash = self.__hash__(previous_block)
-        block = self._create_block(data=data, proof=proof, previous_hash=previous_hash)
+        previous_hash = self._hash(block=previous_block)
+        block = self._create_block(data=data, proof=proof, previous_hash=previous_hash, index=index)
         self.chain.append(block)
         return block
     
@@ -89,3 +89,38 @@ class Blockchain:
     def _hash_block(self, block: dict) -> str:
         encoded_block = _json.dumps(block, sort_keys=True).encode()
         return _hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self) -> bool:
+        
+        current_block = self.chain[0]
+        block_index = 1
+        
+        while block_index < len(self.chain):
+            next_block = self.chain[block_index]
+            
+            if next_block["previous_hash"] != self._hash(current_block):
+                return False
+            
+            current_proof = current_block["proof"]
+            next_index, next_data, next_proof = ( next_block["index"],
+                                                  next_block["data"],
+                                                  next_block["proof"]
+                                                  )
+            
+            hash_value = _hashlib.sha256(
+                self._to_digest(
+                    new_proof=next_proof,
+                    previous_proof=current_proof,
+                    index=next_index,
+                    data=next_data
+                )
+            ).hexdigest()
+            
+            if hash_value[:4] != "0000":
+                return False
+            
+            current_block = next_block
+            block_index += 1
+            
+        return True
+            
