9ab81537011b8913143eed213f2de0250e9e0dec
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-25 21:12:02 +0530

    added mine a block endpoint and return entire blockchain endpoint in main.py

--- fastAPI_blockchain/main.py
+++ fastAPI_blockchain/main.py
@@ -5,4 +5,22 @@ import genesis_block as _blockchain
 blockchain = -_blockchain.Blockchain()
 app = -_fastapi.FastAPI()
 
+# endpoint to mine a block
+@app.post("/mine_block/")
+def mine_block(data: str):
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="blockchain is Invalid")
+    
+    block = blockchain.mine_block(data=data)
+    
+    return block
 
+
+# EP to return blockchain
+@app.get("/blockchain")
+def get_blockchain():
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="blockchain is Invalid")
+    
+    chain = blockchain.chain
+    return chain
