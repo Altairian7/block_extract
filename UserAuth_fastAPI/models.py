ef7f4b41bddfd7c79a69d209a0e77ba0f08b77f0
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-08 23:01:23 +0530

    added Post method in models

--- UserAuth_fastAPI/models.py
+++ UserAuth_fastAPI/models.py
@@ -13,3 +13,12 @@ class User(_database.Base):
     hashed_password = _sql.Column(_sql.String)
     data_created = _sql.column(_sql.DateTime, default=_dt.datetime.utcow)
     
+    
+class Post(_database.Base):
+    __tablename__ = "posts"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
+    post_text = _sql.Column(_sql.String, index=True)
+    data_created = _sql.column(_sql.DateTime, default=_dt.datetime.utcow)
+    
+    owner = _orm.relationship("user", back_populates="posts")
