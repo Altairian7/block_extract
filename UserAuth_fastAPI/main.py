89074d37eec75c1c13ea606fec26e566860bc4fb
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-09 23:40:03 +0530

    imported schemas & services in main.py and created post endpoint

--- UserAuth_fastAPI/main.py
+++ UserAuth_fastAPI/main.py
@@ -2,6 +2,13 @@ import fastapi as _fastapi
 import fastapi.security as _security
 import sqlalchemy.orm as _orm
 
+import schemas as _schemas
+import services as _services
 
 app = _fastapi.FastAPI()
 
+@app.post("/api/users")
+async def create_user(
+    user: _schemas.UserCreate, 
+    db: _orm.Session = _fastapi.Depends(_services.get_db)
+): pass
