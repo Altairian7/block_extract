47f7652b841ab1cccf251b24f759f06fefb516c6
altairian7 <artisharma.as12.as@gmail.com>
2025-03-30 17:22:35 +0530

    create a funciton to calculate hash

--- alt_blockChain.py
+++ alt_blockChain.py
@@ -10,4 +10,13 @@ class Block:
         self.previous_hash = previous_hash
         self.hash = self.calculate_hash()
     
+    def calculate_hash(self):
+        """Generates the hash of the block using SHA-256."""
+        block_string = json.dumps({
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "data": self.data,
+            "previous_hash": self.previous_hash
+        }, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
     
