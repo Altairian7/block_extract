2c6315d26822ed1cc1f926653c71a76aa7d25746
altairian7 <artisharma.as12.as@gmail.com>
2025-04-01 23:07:21 +0530

    created send tranx func

--- eth_main.py
+++ eth_main.py
@@ -14,3 +14,21 @@ def get_eth_balance(address):
     balance_wei = web3.eth.get_balance(address)
     balance_eth = web3.from_wei(balance_wei, 'ether')
     return balance_eth
+
+
+
+def send_eth_transaction(sender, receiver, private_key, amount_eth):
+    nonce = web3.eth.get_transaction_count(sender)
+    gas_price = web3.eth.gas_price  # Get current gas price
+
+    tx = {
+        'nonce': nonce,
+        'to': receiver,
+        'value': web3.to_wei(amount_eth, 'ether'),
+        'gas': 21000,  # Standard gas for ETH transfer
+        'gasPrice': gas_price
+    }
+
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    return web3.to_hex(tx_hash)
