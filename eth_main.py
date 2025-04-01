9d4603a98cd3d77792a2fa93735c277455710139
altairian7 <artisharma.as12.as@gmail.com>
2025-04-01 23:11:36 +0530

    created deploy smart contract func

--- eth_main.py
+++ eth_main.py
@@ -32,3 +32,49 @@ def send_eth_transaction(sender, receiver, private_key, amount_eth):
     signed_tx = web3.eth.account.sign_transaction(tx, private_key)
     tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
     return web3.to_hex(tx_hash)
+
+
+
+
+
+
+
+def deploy_smart_contract(private_key):
+    compiled_contract = {
+        "abi": [
+            {
+                "inputs": [{"internalType": "uint256", "name": "_value", "type": "uint256"}],
+                "name": "store",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function",
+            },
+            {
+                "inputs": [],
+                "name": "retrieve",
+                "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+                "stateMutability": "view",
+                "type": "function",
+            }
+        ],
+        "bytecode": "6080604052348015600f57600080fd5b5060d08061001e6000396000f3fe60806040..."
+    }
+
+    account = web3.eth.account.from_key(private_key)
+    contract = web3.eth.contract(abi=compiled_contract["abi"], bytecode=compiled_contract["bytecode"])
+
+    tx = contract.constructor().build_transaction({
+        'from': account.address,
+        'gas': 3000000,
+        'gasPrice': web3.eth.gas_price,
+        'nonce': web3.eth.get_transaction_count(account.address)
+    })
+
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+    
+    return receipt.contractAddress
+
+
+
